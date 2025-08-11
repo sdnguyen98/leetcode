590070415ff2485ab5d26117a5db71de98cc04de
Steven Nguyen <43599068+sdnguyen98@users.noreply.github.com>
2025-08-11 11:37:24 -0700

    Time: 62 ms (15.44%) | Memory: 53.7 MB (99.99%) - LeetSync

---
+++ 1-two-sum/two-sum.ts
@@ -0,0 +1,11 @@
+function twoSum(nums: number[], target: number): number[] {
+    const results: number[] = []
+    for(let i = 0; i < nums.length; i++){
+        for(let j = 0; j < nums.length; j++){
+            if(nums[i]+nums[j]==target && i!=j){
+                results.push(i, j);
+                return results;
+            }
+        }
+    }
+};
