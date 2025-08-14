e652ebcaf61ea471587b63f7e9419912b4348d10
Steven Nguyen <43599068+sdnguyen98@users.noreply.github.com>
2025-08-14 15:28:27 -0700

    Time: 1 ms (83.21%) | Memory: 58.8 MB (57.97%) - LeetSync

---
+++ 412-fizz-buzz/fizz-buzz.ts
@@ -0,0 +1,12 @@
+function fizzBuzz(n: number): string[] {
+    var res = [];
+    for (var i = 1; i <= n; i++){
+        res.push(
+            (i % 15 === 0) ? "FizzBuzz":
+            (i % 5 === 0) ? "Buzz":
+            (i % 3 === 0) ? "Fizz":
+            i.toString()
+        );
+    }
+    return res;
+};
