8a0e5516a05b0d68b71c00e49d4e45e75f2ee44f
Steven Nguyen <43599068+sdnguyen98@users.noreply.github.com>
2025-08-14 20:30:40 -0700

    Time: 75 ms (10.16%) | Memory: 65.7 MB (97.91%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.js
+++ 217-contains-duplicate/contains-duplicate.js
@@ -3,11 +3,12 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-        nums.sort();
-        for(let i = 1;  i < nums.length; i++){
-            if(nums[i]== nums[i-1]){
-                return true;
-            }
+    nums.sort((a,b) => a - b);
+    for(var i = 1; i < nums.length; ++i){
+        if(nums[i]===nums[i-1]){
+            return true;
         }
-        return false;
+    }
+    return false
+    
 };
