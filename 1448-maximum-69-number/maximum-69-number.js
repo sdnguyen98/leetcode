6e3513781041543a2fcac42d8e48db4ef0a70ad8
Steven Nguyen <43599068+sdnguyen98@users.noreply.github.com>
2025-08-16 14:44:29 -0700

    Time: 0 ms (100.00%) | Memory: 53 MB (86.46%) - LeetSync

---
+++ 1448-maximum-69-number/maximum-69-number.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var maximum69Number  = function(num) {
+
+    let numArray = String(num).split('').map(Number);
+    
+    for(var i =0; i < numArray.length; i++){
+        if(numArray[i]===6){
+            numArray[i] = 9;
+            return Number(numArray.join(''));
+        }
+    }
+    return num;
+
+    
+};
